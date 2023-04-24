5b1165bc387e2bbe0df5065e369de05768a7166a
Eric Levasseur <eric.levass@gmail.com>
2023-04-24 14:46:16 -0400

    dynamic footer year

--- pages/footer.js
+++ pages/footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
 export default function Footer() 
 {
     return (
         <React.Fragment>
             <footer>
-                <p> &copy; 2023 • Eric Levasseur</p>
+                <p> &copy; {currentYear} • Eric Levasseur</p>
             </footer>
         </React.Fragment>
     );
